8479ab41445a4914f1c41c63d69a9ae0ec396ca5
Ipo <61351163ipo-exe@users.noreply.github.com>
2025-07-28 08:18:49 -0300

    slider filter

--- gui.py
+++ gui.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from ipywidgets import interact, IntSlider
-from IPython.display import display
+from IPython.display import display, HTML
 
 def slider_filter(df, column):
     """
@@ -10,7 +10,22 @@ def slider_filter(df, column):
     max_val = int(df[column].max())
 
     def filter_data(min_value, max_value):
+        # Apply filter
         filtered = df[(df[column] >= min_value) & (df[column] <= max_value)]
+
+        # Compute stats
+        count = len(filtered)
+        total_sum = round(filtered[column].sum(), 2)
+
+        # Display summary
+        summary_html = f"""
+            <h4>Resumo</h4>
+            <p><b>Ações filtradas:</b> {count}</p>
+            <p><b>Total filtrado: R$</b> {total_sum}</p>
+            """
+        display(HTML(summary_html))
+
+        # Display filtered data
         display(filtered)
 
     interact(
