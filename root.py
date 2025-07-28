9648036ee4c44422b3baa6eefe8b2c825fe3d0f3
Ipo <61351163ipo-exe@users.noreply.github.com>
2025-07-28 16:45:32 -0300

    action forms

--- root.py
+++ root.py
@@ -1730,7 +1730,8 @@ def gui_filter(db, field):
 
 def gui_form(db):
     from pishne.gui import action_form
-    action_form(db=db, df=join_db(db))
+    d = action_form(db=db, df=join_db(db))
+    return d
 
 
 def export_db2csv(db, use_gui=True, folder=None):
